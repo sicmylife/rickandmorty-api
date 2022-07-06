d747e68fe4079e3ad7e79c476b10cda57a717fd5
Daniel <sicmylife@gmail.com>
2022-07-06 20:16:21 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,103 @@
+
+
+// document.addEventListener("DOMContentLoaded",()=> {
+//     fetchData();
+// });
+
+// const fetchData = async ()=> {
+
+//     try {
+//       loadingData(true);
+
+//       const res = await fetch("	https://rickandmortyapi.com/api/character");
+//       const data = await res.json();
+      
+//         pintarCard(data);
+       
+      
+//     } catch (error) {
+//         console.log(error);
+//     }finally {
+//             loadingData(false);
+//     }
+
+// };
+
+// const pintarCard = data =>{
+//     const cards = document.getElementById('card-dinamicas')
+//     const templateCard = document.getElementById('template-card').content
+//     const fragment = document.createDocumentFragment()
+
+//    data.results.forEach(element => {
+//     // console.log(element);
+
+//     const clone = templateCard.cloneNode(true);
+//     clone.querySelector('h5').textContent=element.name;
+//     clone.querySelector('p').textContent=element.species;
+//     clone.querySelector('img').setAttribute("src",element.image)
+//     // guardamos en el fragment para evitar reflow
+//     fragment.appendChild(clone)
+
+//    });
+
+//    cards.appendChild(fragment)
+ 
+// }
+
+// const loadingData = estado => {
+//     const loading = document.getElementById("loading");
+//     if(true){
+
+//         loading.classList.remove("d-none");
+//     } else {
+        
+//         loading.classList.add("d-none");
+//     }
+// }
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetchData();
+});
+
+const cards = document.querySelector("#card-dinamica");
+const templateCard = document.querySelector("#template-card").content;
+
+const fetchData = async () => {
+    try {
+        loadingData(true);
+
+        const res = await fetch("https://rickandmortyapi.com/api/character");
+        const data = await res.json();
+
+        pintarDatos(data);
+    } catch (error) {
+        console.log(error);
+    } finally {
+        loadingData(false);
+    }
+};
+
+const loadingData = (estado) => {
+    const loading = document.querySelector("#loading");
+    if (estado) {
+        loading.classList.remove("d-none");
+    } else {
+        loading.classList.add("d-none");
+    }
+};
+
+const pintarDatos = (data) => {
+    const fragment = document.createDocumentFragment();
+
+    cards.textContent = "";
+
+    data.results.forEach((item) => {
+        const clone = templateCard.cloneNode(true);
+        clone.querySelector("h5").textContent = item.name;
+        clone.querySelector("p").textContent = item.species;
+        clone.querySelector("img").setAttribute("src", item.image);
+
+        fragment.appendChild(clone);
+    });
+    cards.appendChild(fragment);
+};
